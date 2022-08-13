41c4a96559883f69550d60737a106c4788bfc89b
Artur Zaripov <mithotyn@mail.ru>
2022-08-13 10:33:11 +0300

    Cutted Average

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,19 @@ public class SalesManager {
         }
         return max;
     }
+    public int cutAverage () {
+        int max = sales[0];
+        int min = sales[0];
+        int average = 0;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+            if (sale < min) {
+                min = sale;
+            }
+            average = average + sale;
+        }
+        return (average - min - max) / (sales.length - 2);
+    }
 }
